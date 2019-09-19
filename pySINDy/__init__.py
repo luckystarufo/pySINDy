889d4234b704dd6a164b4b9802faa7776a76f745
daletovar <daletovar@gmail.com>
2019-09-19 12:36:49 -0700

    add discrete-time sindy

--- pySINDy/__init__.py
+++ pySINDy/__init__.py
@@ -3,8 +3,9 @@ pySINDy init
 """
 # pylint: skip-file
 
-__all__ = ['sindybase', 'sindy', 'isindy']
+__all__ = ['sindybase', 'sindy', 'isindy', 'dsindy']
 
 from .sindybase import SINDyBase
 from .sindy import SINDy
 from .isindy import ISINDy
+from .dsindy import DSINDy
